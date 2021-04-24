6d7489ba5365fa1a53ce31e6553168411e84859f
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-04-24 15:32:21 -0500

    [add] login view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 from .models import Account
 from .forms import RegistrationForm
@@ -25,8 +25,8 @@ def register(request):
             )
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration suceesful')
-            return redirect('accounts:register')
+            messages.success(request, "Registration suceesful")
+            return redirect("accounts:register")
             # Create a user profile
     else:
         form = RegistrationForm()
@@ -39,11 +39,24 @@ def register(request):
 
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in.")
+            return redirect("home:home")
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect("accounts:login")
 
     return render(request, "accounts/login.html")
 
 
+@login_required(login_url="accounts:login")
 def logout(request):
     auth.logout(request)
     messages.success(request, "You are logged out.")
-    return redirect("login")
+    return redirect("accounts:login")
