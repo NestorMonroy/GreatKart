a648ca878bbc08c23b8f5798fe01554802ee51b0
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-04-24 11:08:30 -0500

    [update] remove cart view

--- src/carts/views.py
+++ src/carts/views.py
@@ -82,15 +82,16 @@ def add_cart(request, product_id):
 def remove_cart(request, product_id, cart_item_id):
 
     product = get_object_or_404(Product, id=product_id)
-
-    cart = Cart.objects.get(cart_id=_cart_id(request))
-    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
-
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect("cart:home")
 
 
