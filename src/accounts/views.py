eb3055ac10a878744d96f1d617921630a8311938
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-04-26 16:44:22 -0500

    [modify] Cart Counter & Cart View to Handle Logged-in Users & [fix] remove view & cart decrement function & [add] dynamically redirect the user to next page with requests

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
+import requests
+
 # Verification email
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -77,16 +79,52 @@ def login(request):
                 is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exists:
                     cart_item = CartItem.objects.filter(cart=cart)
+                    # Getting the product variations by cart id
+                    product_variation = []
 
                     for item in cart_item:
-                        item.user = user
-                        item.save()
-
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                    
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
             auth.login(request, user)
-            messages.success(request, "You are now logged in.")
-            return redirect("home:home")
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                #print('-->', query)
+                #next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('accounts:dashboard')            
         else:
             messages.error(request, "Invalid login credentials")
             return redirect("accounts:login")
