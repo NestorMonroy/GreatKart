b51a0b3641c6321349800b0b90626e3b1f2dfcec
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-04-28 14:09:20 -0500

    [update] template with rating product

--- src/store/models.py
+++ src/store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Avg, Count
+
 from src.category.models import Category
 from src.accounts.models import Account
 from django.urls import reverse
@@ -18,6 +20,24 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(
+            average=Avg("rating")
+        )
+        avg = 0
+        if reviews["average"] is not None:
+            avg = float(reviews["average"])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(
+            count=Count("id")
+        )
+        count = 0
+        if reviews["count"] is not None:
+            count = int(reviews["count"])
+        return count
 
     def get_url(self):
         return reverse("store:product_detail", args=[self.category.slug, self.slug])
@@ -81,4 +101,3 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
-    
--- src/store/views.py
+++ src/store/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator
 
 from src.category.models import Category
 from src.carts.models import CartItem
+from src.orders.models import OrderProduct
 
 from .models import Product, ReviewRating
 from .forms import ReviewForm
@@ -64,11 +65,25 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
-    ctx = {
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(
+                user=request.user, product_id=single_product.id
+            ).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+    context = {
         "single_product": single_product,
         "in_cart": in_cart,
+        "orderproduct": orderproduct,
+        "reviews": reviews,
     }
-    return render(request, "store/product_detail.html", ctx)
+    return render(request, "store/product_detail.html", context)
 
 
 def store(request, category_slug=None):
