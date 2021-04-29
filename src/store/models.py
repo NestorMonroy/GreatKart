989a822f909878e041503d3bdc5c42cf76132d2b
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-04-29 10:00:28 -0500

    [update] product detail view with multiple photo and jquery

--- src/store/models.py
+++ src/store/models.py
@@ -43,18 +43,6 @@ class Product(models.Model):
         return reverse("store:product_detail", args=[self.category.slug, self.slug])
 
 
-class ProductGallery(models.Model):
-    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="store/products", max_length=255)
-
-    def __str__(self):
-        return self.product.product_name
-
-    class Meta:
-        verbose_name = "productgallery"
-        verbose_name_plural = "product gallery"
-
-
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(
@@ -101,3 +89,15 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
+
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="store/products", max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = "productgallery"
+        verbose_name_plural = "product gallery"
