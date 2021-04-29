059da607adf66433ecacf234b17fdc60f31be09f
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-04-29 10:08:33 -0500

    [update]  home view with rating stars

--- src/home/views.py
+++ src/home/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+
+from src.store.models import Product, ReviewRating
 from src.store.models import Product
 
 
 def home(request):
-    path = "home.html"
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by("created_date")
 
-    ctx = {"products": products}
+    # Get the reviews
+    reviews = None
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
 
-    return render(request, path, ctx)
+    ctx = {
+        "products": products,
+        "reviews": reviews,
+    }
+    return render(request, "home.html", ctx)
